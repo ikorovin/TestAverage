9ab7c9bc6a3d0873b96a8dd0a79d120d855fda06
Igor Korovin <54861554+ikorovin@users.noreply.github.com>
2019-09-06 13:35:51 -0500

    Average score of 3

--- TestAverage/TestAverage/Program.cs
+++ TestAverage/TestAverage/Program.cs
@@ -10,6 +10,23 @@ namespace TestAverage
     {
         static void Main(string[] args)
         {
+            int score1;
+            int score2;
+            int score3;
+            double average;
+
+            Console.WriteLine("What is your first test score?");
+            score1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your second test score?");
+            score2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your third test score?");
+            score3 = Convert.ToInt32(Console.ReadLine());
+            var addition = (score1 + score2 + score3);
+            average = addition / 3;
+            Console.WriteLine("Average score is " + average);
+            Console.ReadKey();
+
+            
         }
     }
 }
